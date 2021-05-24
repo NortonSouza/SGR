45808fc6cba3f3c2ce25376d1f28371273b84af6
ArthurHSX <43755536+ArthurHSX@users.noreply.github.com>
2021-05-23 21:06:33 -0300

    login implementado

--- js/app/home.js
+++ js/app/home.js
@@ -2,3 +2,28 @@ var url_Api = "https://image.tmdb.org/t/p/w200/";
 var rota_sala = "api/sala/";
 var rota_equipamento = "api/equipamento/";
 var rota_reuniao = "api/reuniao/";
+var idsessao = "";
+
+function ValidaSessao() {    
+    idsessao = sessionStorage.getItem("key");
+    if (idsessao == "")
+        window.location.href = "login.html";
+    else
+    {
+        var request = new XMLHttpRequest();
+
+        request.open('GET', 'https://localhost:44378/api/Sessao/' + idsessao, true);
+
+        request.setRequestHeader('Content-Type', 'application/json');        
+        
+        request.onreadystatechange = function () {
+            if (this.readyState === 4) {                
+                if(request.status !== 200){                    
+                    window.location.href = "login.html"; 
+                }
+            }
+        };
+        
+        request.send(); 
+    }
+}
--- js/app/login.js
+++ js/app/login.js
@@ -2,13 +2,25 @@ function login() {
     var senha = document.getElementById("password").value;
     var usuario = document.getElementById("username").value;
     
-    //alterar para chamada api
-    var usuarioSenhaValidos = usuario === "joao" && senha === "joao";
+    var request = new XMLHttpRequest();
 
-    if(usuarioSenhaValidos == true){
-        window.location.href = "Home.html";
-    }
-    else{
-        alert("Usuario/Senha invalidos")
-    }
+    request.open('POST', 'https://localhost:44378/api/Usuario/CreateSessao', true);
+
+    request.setRequestHeader('Content-Type', 'application/json');        
+    
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if(request.status === 200){                
+                var obj = JSON.parse(this.responseText)                
+                sessionStorage.setItem("key", obj.guid);                                
+                window.location.href = "Home.html";
+            }else{
+                console.log("Erro");
+                console.log(request);
+                alert("Usuario/Senha invalidos")
+            }
+        }
+    };
+    
+    request.send(JSON.stringify({"login": usuario, "senha": senha}));
   };
